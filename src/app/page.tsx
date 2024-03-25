5955a3005bec902698ddfc62ebd95a7cf5e691f9
usama <ua.usama.asfar@gmail.com>
2024-03-25 13:40:22 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen justify-center items-center p-24">
+      <h1 className="text-5xl font-bold">The Nodestream Foundation</h1>
+    </main>
+  );
+}
