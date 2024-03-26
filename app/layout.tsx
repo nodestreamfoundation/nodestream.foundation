0a36e9c4fed08e2da75a560c76153f0a61279ecb
usama <ua.usama.asfar@gmail.com>
2024-03-26 11:47:39 +0530

    refactor: setup vercel analytics and speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
